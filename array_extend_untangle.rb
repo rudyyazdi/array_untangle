5e1990ecf88a8fc1375b988d1b56d27f27b67b83
Rudy <rudyyazdi@gmail.com>
2016-06-03 15:51:16 +1000

    first commit

---
+++ array_extend_untangle.rb
@@ -0,0 +1,32 @@
+class Array
+  # This is an awesome handy method which turn nested arrays into multiple
+  # flat arrays.
+  # For non-nested arrays it just reruns an array with one item.
+  # [1,2].untangle -> [[1, 2]]
+  # For nested array it creates a new array for each members of the nested member.
+  # [[1,3],4].untangle -> [[1, 4], [3, 4]]
+  # [[1,2],3,[4,5,6]].untangle ->
+  # [[1, 3, 4], [1, 3, 5], [1, 3, 6], [2, 3, 4], [2, 3, 5], [2, 3, 6]]
+
+  def untangle
+    def to_a(item)
+      if item.is_a? Array
+        item
+      else
+        [item]
+      end
+    end
+
+    results = to_a(self.shift).repeated_permutation(1).to_a
+
+    self.each do |member|
+      results = results.product(to_a(member))
+
+      results.map! do |result|
+        result.flatten!
+      end
+    end
+
+    results
+  end
+end
